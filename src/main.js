8927257a36a9cdf1fe8ef3def52565baf8b8dd47
Mikko Martinez <martinezmikko2001@gmail.com>
2023-09-25 16:44:32 -0700

    import bootstrap to main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/js/index.umd.js'
 createApp(App).mount('#app')
